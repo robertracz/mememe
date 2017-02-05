a32f094d859b1c51f474493bef753bf7f1bfe9f6
Robert Racz <robertbracz@gmail.com>
2017-02-05 16:58:29 -0600

    initial commit

---
+++ mememe/mememe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mememe
+//
+//  Created by Robert Bence Racz on 2/5/17.
+//  Copyright © 2017 Robert Bence Racz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
